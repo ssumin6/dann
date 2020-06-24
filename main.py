025c90b57c45b3157af77c6a9334c80fa378cfa1
ssumin6 <ssumin6@gmail.com>
2020-06-25 01:48:52 +0900

    DANN: overall form completed, but train not working

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision import transforms
+from torch.autograd import Variable
 
-import math
+import numpy as np
 
 import argparse
 from models import *
@@ -55,16 +56,19 @@ def main(args, ITE=0):
 
     # train
     for epoch in range(1, args.epochs+1):
-
-        train_length = min(len(svhn_train), len(mnist_train))
-        svhn_train_iter = iter(svhn_train_loader)
-        mnist_train_iter = iter(mnist_train_loader)
         
-        i = 0
-        while i < train_length:
+        start_steps = (epoch-1)*len(svhn_train_loader)
+        total_steps = args.epochs *len(svhn_train_loader)
+        
+        for batch_idx, (sdata, tdata) in enumerate(zip(svhn_train_loader, mnist_train_loader)):
+            # update domain classifier
+            p = (batch_idx+ start_steps) / total_steps
+            lr = args.lr / ((1+alpha*p)**bta)
+            lmda = 2. / (1. + np.exp(-args.gamma*p)) -1
+
             # dataset loading
-            svhn_img, svhn_label = svhn_train_iter.next()
-            mnist_img, _ = mnist_train_iter.next()
+            svhn_img, svhn_label = sdata
+            mnist_img, _ = tdata
 
             svhn_img = svhn_img.to(device)
             mnist_img = mnist_img.to(device)
@@ -73,56 +77,77 @@ def main(args, ITE=0):
             optimizer.zero_grad()
 
             # Classifier training 
-            yf = feature_extractor(svhn_img)
-            y = classifier(yf.view([args.batch_size, -1]))
-            
-            C_loss = classifier_criterion(y, svhn_label)
+            svhn_feature = feature_extractor(svhn_img)
+            svhn_pred = classifier(svhn_feature)
+            C_loss = classifier_criterion(svhn_pred, svhn_label)
 
-            # Discriminator 
-
+            # Discriminator
             # Domain adaptation regularizer from current domain 
-            yf = feature_extractor(svhn_img)
-            y = discriminator(yf.view([args.batch_size, -1]))
+            svhn_feature2 = feature_extractor(svhn_img)
+            src_pred = discriminator(svhn_feature2, lmda)
 
-            d_label = torch.zeros(args.batch_size).long().to(device)
-            src_loss = discriminator_criterion(y, d_label)
+            d_label = Variable(torch.zeros(args.batch_size).long().to(device))
+            
+            src_loss = discriminator_criterion(src_pred, d_label)
 
             # ... from target domain.
-            yf = feature_extractor(mnist_img)
-            y = discriminator(yf.view([args.batch_size, -1])) 
+            tgt_feature = feature_extractor(mnist_img)
+            tgt_pred = discriminator(tgt_feature, lmda) 
 
-            d_label = torch.ones(args.batch_size).long().to(device)
-            target_loss = discriminator_criterion(y, d_label)
+            d_label2 = Variable(torch.ones(args.batch_size).long().to(device))
+            tgt_loss = discriminator_criterion(tgt_pred, d_label2)
 
-            D_loss = lmda*(src_loss + target_loss)
+            D_loss = src_loss + tgt_loss
             loss = C_loss + D_loss
             loss.backward()
 
             # update parameter
             optimizer.step()
 
-            if (i % 100 == 0):
-                print("Epoch %d: [%d itr] C_loss: %f, D_loss: %f" %(epoch, i, C_loss, D_loss))
-                
-            # update domain classifier
-            p = (i+ epoch*train_length) / (args.epochs *train_length)
-            lr = args.lr / ((1+alpha*p)**bta) # TODO 
-            lmda = 2 / (1+ math.exp(-args.gamma*p)) -1
-            i += 1 
+            if ((batch_idx+start_steps) % 100 == 0):
+                print("Epoch %d: [%d itr] C_loss: %f, D_loss: %f" %(epoch, batch_idx+start_steps, C_loss.item(), D_loss.item()))
 
-            # TODO : test!
+        test(feature_extractor, classifier, svhn_test_loader, device, "svhn")
+        test(feature_extractor, classifier, mnist_test_loader, device, "mnist")
 
-        torch.save({
-        'feature' : feature_extractor.state_dict(),
-        'discriminator' : discriminator.state_dict(),
-        'classifier' : classifier.state_dict(),
-        'optimF' : optimF.state_dict(),
-        'optimD' : optimD.state_dict(),
-        'paramC' : optimC.state_dict(),
-        'args' : args
-        }, 'drive/My Drive/checkpoint/model_epoch{}'.format(epoch))
+        if (epoch % 25 == 0):
+            torch.save({
+            'feature' : feature_extractor.state_dict(),
+            'discriminator' : discriminator.state_dict(),
+            'classifier' : classifier.state_dict(),
+            'optim' : optimizer.state_dict(),
+            'args' : args
+            }, 'drive/My Drive/checkpoint/model_epoch{}'.format(epoch))
 
-# def test():
+def test(feature, model, data_loader, device, name):
+    feature.eval()
+    model.eval()
+    n_total = 0 
+    n_correct = 0 
+    
+    test_iter = iter(data_loader)
+    num = len(data_loader)
+
+    i = 0 
+    while i < num:
+        img, label = test_iter.next()
+        # Get batch size
+        batch_size = len(label)
+        # Transfer data tensor to GPU/CPU (device)
+        img = img.to(device)
+        label = label.to(device)
+
+        ft = feature(img)
+        pred = model(ft.view(batch_size, -1))
+        pred = pred.data.max(1, keepdim=True)[1]
+        n_correct += pred.eq(label.data.view_as(pred)).cpu().sum()
+        n_total += batch_size
+        i += 1
+
+    acc = n_correct.data.numpy() * 1.0 / n_total
+
+    print("##########")
+    print("TEST data : %s accuracy : %f" %(name, acc))
 
 
 if __name__=="__main__":
--- models.py
+++ models.py
@@ -3,22 +3,37 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Function
 
-    
+class GRL(Function):
+    @staticmethod
+    def forward(ctx, x, lmda):
+        ctx.lmda = lmda
+        return x
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        output = grad_output.neg() *ctx.lmda
+
+        return output, None
+
 # Feature Extractor
 class FeatureExtractor(nn.Module):
     def __init__(self):
         super(FeatureExtractor, self).__init__()
         self.conv1 = nn.Conv2d(1, 64, 5, padding=3)
+        self.bn1 = nn.BatchNorm2d(64)
         self.pool = nn.MaxPool2d(3, 2)
         self.conv2 = nn.Conv2d(64, 64, 5, padding=3)
+        self.bn2 = nn.BatchNorm2d(64)
         self.conv3 = nn.Conv2d(64, 128, 5, padding=1)
         
     def forward(self, x):
         # train on SVHN
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        batch_size = x.data.shape[0]
+        # x = x.expand(batch_size, 1, 28, 28)
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
         x = F.relu(self.conv3(x))
-        return x
+        return x.view(batch_size, -1)
 
 # Classifier
 class Classifier(nn.Module): 
@@ -32,7 +47,7 @@ class Classifier(nn.Module):
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.softmax(self.fc3(x))
+        x = F.log_softmax(self.fc3(x), 1)
         return x
 
 # Discriminator
@@ -41,22 +56,11 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
         self.fc1 = nn.Linear(128*5*5, 1024)
         self.fc2 = nn.Linear(1024, 1024)
-        self.fc3 = nn.Linear(1024, 1)
+        self.fc3 = nn.Linear(1024, 2)
         
-    def forward(self, x):
-        x = GRL.apply(x)
+    def forward(self, x, lmda):
+        x = GRL.apply(x, lmda)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.softmax(self.fc3(x), dim=1)
+        x = F.log_softmax(self.fc3(x), 1)
         return x
-
-class GRL(Function):
-    @staticmethod
-    def forward(ctx, x):
-        return x
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        output = grad_output.neg() 
-
-        return output, None
